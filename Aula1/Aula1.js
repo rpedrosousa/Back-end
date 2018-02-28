72221468466f833080a670434f5d7d359f82c7e1
Pedro <pedrosousa.pass@gmail.com>
2018-02-28 20:45:21 +0000

    aula1

---
+++ Aula1/Aula1.js
@@ -0,0 +1,12 @@
+function med(){
+    var nota1=13.6;
+    var nota2=14.4;
+    var nota3=11.5;
+    med =(nota1+nota2+nota3)/3
+    if (med <9.5){
+        console.log("Reprovado")
+    }
+    else{
+        console.log("Aprovado")
+    }
+}
