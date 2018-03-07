1a4d5ed1f50d3276a05889a026b7e15cc7964019
Pedro <pedrosousa.pass@gmail.com>
2018-03-07 21:23:09 +0000

    aula2 ficha2 com dados

--- Aula2/ficha2.js
+++ Aula2/ficha2.js
@@ -1,11 +1,14 @@
+//EX1 - Escreva uma função que calcule o índice de massa corporal de uma pessoa e imprima a sua condição física. A fórmula contém o peso em kg e a altura em metros
+
+
 function calculo(peso, altura){
     return peso/(altura*altura);
 
 }
 
 function imc(){
-var peso = 70;
-var altura = 1.50;
+var peso = 80;
+var altura = 1.75;
 var imc1 = calculo(peso,altura);
     if(imc1<18.5){
         console.log("o seu imc é "+ imc1 +": Abaixo do peso ");
