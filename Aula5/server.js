033e234a6597748cf45cad803187decd514b7f97
Pedro <pedrosousa.pass@gmail.com>
2018-04-18 18:39:20 +0100

    aula 5 ficha 5 feita
    aula 5 ficha 5 feita

--- Aula5/server.js
+++ Aula5/server.js
@@ -4,11 +4,11 @@ const bodyParser=require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-var fs= require('fs');
+var fs= require('fs');//fs = fyle system
 
 function readFile(fileName){
-    var file = fs.readFileSync(fileName,'utf-8');
-    return file;
+    var ficha = fs.readFileSync(fileName,'utf-8');
+    return ficha;
 }
 
 app.get('/listPerson', function(request,response){
@@ -20,13 +20,25 @@ app.post('/addPerson', function(request,response){
     var file= readFile("./person.json");
     var jsonData= JSON.parse(file);
     var size= Object.keys(jsonData).length;
-    var id = size++;
+    var id = ++size;
     var p = request.body;
-    console.log(p);
-
     p.id=id;
     jsonData["person" + id] = p;
     response.send(jsonData);
+});
+
+app.delete('/deletePerson', function(request,response){
+    var del=readFile("./person.json");
+    var jsonData= JSON.parse(del);
+    delete jsonData[ 'person' + request.body.id];
+    response.send(jsonData);
+});
+
+app.get('/selPerson/:id',function(request,response){
+    var sel=readFile("./person.json");
+    var jsonData=JSON.parse(sel);
+    var person= jsonData['person' + request.params.id];
+    response.send(person);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
